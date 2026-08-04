6c7a6760e2d3e2818d9951e64cdf1061599abb90
Sebastien Rousseau <sebastian.rousseau@gmail.com>
2026-08-04 17:43:21 +0000

    draft: bump to 0.0.31

--- Casks/draft.rb
+++ Casks/draft.rb
@@ -1,24 +1,24 @@
 # This file is maintained by .github/workflows/bump-draft.yml.
 cask "draft" do
-  version "0.0.30"
+  version "0.0.31"
 
   on_macos do
     on_arm do
-      sha256 "ff1f4a9c8b007bb8e933b6251c495edbb2e0ca035360a0a0dab4ae1157f1326d"
+      sha256 "5c4fa5021385d12b3dd8c155abe617744f42078a329e829f9f750499b115f3db"
       url "https://github.com/sebastienrousseau/draft/releases/download/v#{version}/draft_#{version}_darwin_arm64.tar.gz"
     end
     on_intel do
-      sha256 "03841c7994be1b113325d40170601575e128681bf28cd1a879fde85429b45cf1"
+      sha256 "478af9053d8cbbaeeb534c98dffe83c1bb38bffbbdb83ca8b137ad4c659e9555"
       url "https://github.com/sebastienrousseau/draft/releases/download/v#{version}/draft_#{version}_darwin_amd64.tar.gz"
     end
   end
   on_linux do
     on_arm do
-      sha256 "42a751703d27d7641803f8a742fe43b6663e4813b7fcf3d643a6061654229780"
+      sha256 "7bd759e4c44ccff0dd021f2378dd6a0fa9e8f2147e8a00beebd4b97120aedc18"
       url "https://github.com/sebastienrousseau/draft/releases/download/v#{version}/draft_#{version}_linux_arm64.tar.gz"
     end
     on_intel do
-      sha256 "7174faa9833d6c3bd406de0c71022fe2f9944c353e712ba2f7af3180babfdba4"
+      sha256 "43c2a72b724ac8f53b176ea8e1ce344c54e02290c751c22e26e8481d6c18f9a6"
       url "https://github.com/sebastienrousseau/draft/releases/download/v#{version}/draft_#{version}_linux_amd64.tar.gz"
     end
   end
